428973c582b02d40b977593666d7bf059b75592b
JOÃO PAULO ARAUJO CAPPELETTI <joao_cappeletti@estudante.sesisenai.org.br>
2024-10-24 11:01:58 -0300

    salvo

--- create-table.js
+++ create-table.js
@@ -1,8 +1,8 @@
 import { sql } from './db.js'
 
 sql`
-  CREATE TABLE usuarios(
-      id text PRIMARY KEY,
+  CREATE TABLE Usuarios(
+      id_usuario text PRIMARY KEY,
       nome character varying(255),
       email character varying(255),
       senha character varying(255),
@@ -11,19 +11,29 @@ sql`
       endereco character varying(255)
   );
 
-  CREATE TABLE produtos(
-      id text PRIMARY KEY,
+  CREATE TABLE Produtos(
+      id_produto text PRIMARY KEY,
       tamanho character varying(5),
       valor text,
       quantidade int,
       descricao character varying(255)
   );
 
-  CREATE TABLE vendas(
-      id text PRIMARY KEY,
-      id_usuario FOREIGN KEY,
-      status_venda BOOLEAN ,
-      valor_venda int
+  CREATE TABLE Vendas(
+      id_venda text PRIMARY KEY,
+      id_usuario text,
+      FOREIGN KEY (id_usuario) REFERENCES Usuarios (id_usuario),
+      status_venda BOOLEAN,
+      metodo_pagamento character varying(255)
+      valor_venda text
+  );
+
+  CREATE TABLE VendasProduto(
+      id_vendasProduto text PRIMARY KEY,
+      id_produto text,
+      FOREIGN KEY (id_produto) REFERENCES Produtos (id_produto),
+      id_venda text,
+      FOREIGN KEY (id_venda) REFERENCES Vendas (id_venda),
   );
 `.then(() => {
   console.log('tabela criada');
